0e0fe6ff942a21a1c305cd307742456f95f0fd77
BW3538 <braden.mcw@gmail.com>
2020-05-19 13:49:39 +0200

    Implemented base world system
    Created infinte loading of pooled land, imported dinosaur and plant assets.

---
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField]
+    int groundPool = 3;
+
+    [SerializeField]
+    GameObject ground;
+
+    List<GameObject> groundToSpawn;
+
+    [SerializeField]
+    Vector3 spawnPosition = new Vector3(-45, 0, 0);
+    [SerializeField]
+    Vector3 endPosition = new Vector3(30, 0, 0);
+    [SerializeField]
+    Vector3 deletePoint = new Vector3(60, 0, 0);
+
+    GameObject currentGround;
+    GameObject oldGround;
+
+    void Awake()
+    {
+
+        groundToSpawn = new List<GameObject>();
+        for (int i = 0; i < groundPool; i++)
+        {
+            //Instantiate(ground, spawnPosition, Quaternion.identity);
+            GameObject temp = (GameObject)Instantiate(ground);
+            temp.SetActive(false);
+            groundToSpawn.Add(temp);
+        }
+    }
+
+    void Start()
+    {
+        for(int i = 0; i <1; i++)
+        {
+            currentGround = groundToSpawn[i];
+            currentGround.transform.position = new Vector3(-45,0,0);
+            groundToSpawn[i].SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+
+        if(currentGround.transform.position.x >= endPosition.x)
+        {
+            foreach (var newGround in groundToSpawn)
+            {
+                if (!newGround.activeInHierarchy)
+                {
+                    oldGround = currentGround;
+                    newGround.transform.position = spawnPosition;
+                    newGround.SetActive(true);
+                    currentGround = newGround;
+                    break;
+                }
+            }
+        }
+
+        if (oldGround != null)
+        {
+            if(oldGround.transform.position.x >= deletePoint.x)
+            {
+                oldGround.SetActive(false);
+            }        
+        }      
+    }
+}
---
+++ Assets/Scripts/Level/LevelMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class LevelMove : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+
+    Vector3 endPosition = new Vector3(45, 0, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(2, 0) * speed * Time.deltaTime);
+    }
+
+
+}
