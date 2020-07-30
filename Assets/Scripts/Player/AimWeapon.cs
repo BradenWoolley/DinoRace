b892a6d40f6d41af8e29a711f559c0a95d4424b6
BW3538 <braden.mcw@gmail.com>
2020-07-30 22:45:09 +0200

    UI Control
    Added UI Control to rotate the player with a mobile device.

--- Assets/Scripts/Player/AimWeapon.cs
+++ Assets/Scripts/Player/AimWeapon.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class AimWeapon : MonoBehaviour
 {
-    [SerializeField] float rotateSpeed;
+    [SerializeField] float rotateSpeed, crossHairSpeed;
+    GameObject crossHair;
+
+    public Joystick joystick;
     // Start is called before the first frame update
     void Start()
     {
-        
+        crossHair = GameObject.Find("CrossHair");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+
+        transform.Rotate(new Vector3(0, joystick.Horizontal * rotateSpeed * Time.deltaTime));
+        crossHair.transform.Translate(crossHairSpeed * joystick.Horizontal * rotateSpeed * Time.deltaTime, 0, 0);
+
+        /*if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime));
+            crossHair.transform.Translate(crossHairSpeed *-rotateSpeed * Time.deltaTime, 0 , 0);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime));
-        }
+            crossHair.transform.Translate(crossHairSpeed * rotateSpeed * Time.deltaTime, 0, 0);
+        }*/
     }
 }
